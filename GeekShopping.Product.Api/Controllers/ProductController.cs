6f609e9e51f66a999d6cd0e856be5fe979bb702a
Bruno <Brunoachon@hotmail.com>
2026-02-25 15:37:36 -0300

    Adicionar arquivos de projeto.

---
+++ GeekShopping.Product.Api/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using GeekShopping.ProductApi.Data.ValueObjects;
+using GeekShopping.ProductApi.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+        public ProductController(IProductRepository repository)
+        {
+            _productRepository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _productRepository.FindAll();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _productRepository.FindById(id);
+            if (product.Id <= 0) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        {
+            if (vo == null) return BadRequest();
+
+            var product = await _productRepository.Create(vo);
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        {
+            if (vo == null) return BadRequest();
+
+            var product = await _productRepository.Update(vo);
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<ProductVO>> Delete(long id)
+        {
+            var status = await _productRepository.Delete(id);
+            if (!status) return BadRequest();
+
+            return Ok(status);
+        }
+    }
+}
---
+++ GeekShopping.Product.Api/Startup.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using GeekShopping.ProductApi.Config;
+using GeekShopping.ProductApi.Model.Context;
+using GeekShopping.ProductApi.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using MySqlConnector;
+
+namespace GeekShopping.ProductApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // Aqui vai o builder.Services
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // MySql
+            var con = Configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySqlContext>(options => options
+                .UseMySql(con, new MySqlServerVersion(new Version(5,0,10))));
+
+            // AutoMapper
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            services.AddSingleton(mapper);
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            // Scopes
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            // Controllers
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            
+            // Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductApi", Version = DateTime.UtcNow.ToString() });
+            });
+        }
+
+        // Aqui vai o pipeline (app.Use...)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+
+}
